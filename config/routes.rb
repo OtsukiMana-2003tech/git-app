a67ab390193e0ab257943af9b807d26f7667073c
OtsukiMana-2003tech <rstoyo3.otsuki.mana@techcamp-training.com>
2024-04-20 14:21:32 +0900

    posts#indexへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root 'posts#index'
+  end
 end
